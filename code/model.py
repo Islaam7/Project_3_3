37ed99d5c26d16b7f54275d00c260080e7e262a9
MohamedAhmed342 <mohamedtemo342@gmail.com>
2025-12-14 15:57:16 +0200

    Finalize simple CNN architecture

--- code/model.py
+++ code/model.py
@@ -1,25 +1,25 @@
 from tensorflow.keras import layers, models
 
 
-def build_simple_cnn(input_shape=(128, 128, 3), num_classes=38):
+def build_simple_cnn(input_shape = (128, 128, 3), num_classes=38):
     model = models.Sequential([
         layers.Input(shape=input_shape),
 
-        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
-        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(32, (3,3), activation='relu', padding='same'),
+        layers.Conv2D(32, (3,3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2,2)),
 
-        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3,3), activation='relu', padding='same'),
+        layers.Conv2D(64, (3,3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2,2)),
 
-        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
-        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3,3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2,2)),
 
         layers.Flatten(),
         layers.Dense(256, activation='relu'),
         layers.Dropout(0.5),
         layers.Dense(num_classes, activation='softmax')
-
-    ])
+        ])
     return model
+
