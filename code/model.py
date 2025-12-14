59329392a359746e5082cba895f3aeaaaef6164d
MohamedAhmed342 <mohamedtemo342@gmail.com>
2025-12-14 15:56:14 +0200

    Add convolutional and pooling layers for feature extraction

--- code/model.py
+++ code/model.py
@@ -4,5 +4,17 @@ from tensorflow.keras import layers, models
 def build_simple_cnn(input_shape=(128, 128, 3), num_classes=38):
     model = models.Sequential([
         layers.Input(shape=input_shape),
+
+        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D((2, 2)),
+
     ])
     return model
